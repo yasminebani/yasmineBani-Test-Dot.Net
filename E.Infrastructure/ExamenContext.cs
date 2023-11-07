faab3ac5437398a577d0968e2277ecbb8e96a4f8
Bani Yasmine <bani.yasmine99@gmail.com>
2023-11-07 23:38:40 +0100

    done

---
+++ E.Infrastructure/ExamenContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E.Infrastructure
+{
+    public class ExamenContext:DbContext
+    {
+        //public DbSet<>  { get; set; }
+        //public DbSet<>  { get; set; }
+        //public DbSet<>  { get; set; }
+        //public DbSet<>  { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+//            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\mssqllocaldb;
+//Initial Catalog=YasmineBani;Integrated Security=true");
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+          //  modelBuilder.ApplyConfiguration(new ());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            base.ConfigureConventions(configurationBuilder);
+        }
+    }
+}
